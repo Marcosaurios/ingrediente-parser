0d61c9d86c30850a832b2cc3ceb11b9c7c454f21
Marcos Urios <marcosuriosgomez@gmail.com>
2022-01-08 13:56:03 +0100

    feat: accented ingredients and empty amount added

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,15 +1,42 @@
 import * as parser from "../ingredientes-parser"
 
   describe("Basic parsing", () => {
+
     test('should read simplest ingredient OK', () => {
       const r = parser.parse("20 pepinos")
       expect(r.amount).toBe("20")
     })
-    test('should read multiple spaced ingredient OK', () => {
+
+    test('should read spaced ingredient OK', () => {
       const r = parser.parse("2  naranjas")
       expect(r).toStrictEqual({
         amount: "2",
         ingredient: "naranjas"
       })
     })
+
+    test('should read an empty ingredient OK', () => {
+      const r = parser.parse("naranjas")
+      expect(r).toStrictEqual({
+        amount: null,
+        ingredient: "naranjas"
+      })
+    })
+
+    test('should read an empty ingredient accented OK', () => {
+      const r = parser.parse("limón")
+      expect(r).toStrictEqual({
+        amount: null,
+        ingredient: "limón"
+      })
+    })
+
+    test('should read ingredient accented OK', () => {
+      const r = parser.parse("límónádúróé por ración de ñora")
+      expect(r).toStrictEqual({
+        amount: null,
+        ingredient: "límónádúróé"
+      })
+    })
+
   })
--- tests/units.spec.js
+++ tests/units.spec.js
@@ -3,36 +3,33 @@ import * as parser from "../ingredientes-parser"
 describe("Units parsing tests", () => {
   
   describe("Simple units", () => {
-    test('should read multiple amounts OK', () => {
-      const r = parser.parse("2 docenas de naranja")
-      console.log(r)
+
+    test('should read empty amounts OK', () => {
+      const r = parser.parse("naranja")
       expect(r).toStrictEqual({
-        amount: "2 docenas",
+        amount: null,
         ingredient: "naranja"
       })
     })
 
     test('should read units OK', () => {
       const r = parser.parse("2 kg de naranja")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "2 kg",
         ingredient: "naranja"
       })
     })
 
-    test('should read units without space OK', () => {
+    test('should read kg units without space OK', () => {
       const r = parser.parse("2kg de naranja")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "2kg",
         ingredient: "naranja"
       })
     })
 
-    test('should read other units OK', () => {
+    test('should read liters units OK', () => {
       const r = parser.parse("2L de naranja")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "2L",
         ingredient: "naranja"
@@ -41,16 +38,22 @@ describe("Units parsing tests", () => {
 
     test('should read other units strict OK', () => {
       const r = parser.parse("2L. de leche")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "2L.",
         ingredient: "leche"
       })
     })
 
-    test('should read diferent units OK', () => {
+    test('should read custom amounts OK', () => {
+      const r = parser.parse("2 docenas de naranja")
+      expect(r).toStrictEqual({
+        amount: "2 docenas",
+        ingredient: "naranja"
+      })
+    })
+
+    test('should read more custom amounts OK', () => {
       const r = parser.parse("50 cucharadas de aceite")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "50 cucharadas",
         ingredient: "aceite"
@@ -61,34 +64,30 @@ describe("Units parsing tests", () => {
   describe("Fraction units parsing", () => {
     test('should read fraction amounts OK', () => {
       const r = parser.parse("1/4 tomate")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "1/4",
         ingredient: "tomate"
       })
     })
   
-    test('should read fraction compound amounts OK', () => {
+    test('should read fraction amount + preposition OK', () => {
       const r = parser.parse("1/4 de tomate")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "1/4",
         ingredient: "tomate"
       })
     })
   
-    test('should read fraction units amounts OK', () => {
+    test('should read fraction + units amounts OK', () => {
       const r = parser.parse("1/2 kg de tomate")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "1/2 kg",
         ingredient: "tomate"
       })
     })
   
-    test('should read fraction units without space OK', () => {
+    test('should read fraction + units without space OK', () => {
       const r = parser.parse("1/2kg de tomate")
-      console.log(r)
       expect(r).toStrictEqual({
         amount: "1/2kg",
         ingredient: "tomate"
